fc4232257cc186b00cdd8ec0ab814b24f4943d86
Abigail Ezell <maezell@gmail.com>
2026-03-10 21:41:31 -0500

    working through number 10

--- Lahman_Exercise_AE.sql
+++ Lahman_Exercise_AE.sql
@@ -117,4 +117,105 @@ where yearid between 1970 and 2016
 and WSWin = 'Y'))
 group by yearid, teamid
 having count(*) = 2;
--- There were 10 years between 1970 and 2016 where the team with the most wins also won the world serie.
+-- There were 10 years between 1970 and 2016 where the team with the most wins also won the world series.
+
+
+/* 6. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? 
+ Give their full name and the teams that they were managing when they won the award. */
+
+select p.namefirst || ' ' || p.namelast as managername, a.awardid, a.yearid, a.lgid, t.name
+from AwardsManagers a
+inner join managers m
+using(playerid, yearid)
+inner join teams t
+using(yearid, teamid)
+inner join people p 
+using(playerid)
+where a.awardid = 'TSN Manager of the Year' 
+and a.lgid in ('NL', 'AL')
+and a.playerid in 
+	(select distinct playerid
+	from AwardsManagers
+	where awardid = 'TSN Manager of the Year' and lgid in ('NL', 'AL')
+	group by playerid
+	having count(distinct lgid) = 2)
+order by managername, yearid;
+
+/* 7. Which pitcher was the least efficient in 2016 in terms of salary / strikeouts? Only consider pitchers who started at least 10 games (across all teams). 
+ Note that pitchers often play for more than one team in a season, so be sure that you are counting all stats for each player. */
+select pe.namefirst || ' ' || pe.namelast as playername, round(sum(cast(s.salary as numeric))/sum(cast(p.so as numeric)), 2) as efficiency
+from pitching p
+inner join salaries s
+using(playerid)
+inner join people pe
+using(playerid)
+where p.yearid = 2016
+group by playerid, playername
+having min(gs) >= 10
+order by efficiency
+limit 1;
+-- Robbie Ray was the least efficient pitcher in 2016 in terms of salary / strikeouts.
+
+/* 8. Find all players who have had at least 3000 career hits. Report those players' names, total number of hits, and the year they were inducted into the hall of fame 
+(If they were not inducted into the hall of fame, put a null in that column.) Note that a player being inducted into the hall of fame is indicated by a 'Y' 
+in the inducted column of the halloffame table. */
+with halloffamers as 
+	(select playerid, yearid
+	from HallOfFame
+	where inducted = 'Y')
+select p.namefirst || ' ' || p.namelast as playername, sum(b.H) as careerhits, halloffamers.yearid as yearinducted
+from batting b
+inner join people p
+using(playerid)
+left join halloffamers
+using(playerid)
+group by playerid, playername, halloffamers.yearid
+having sum(b.H) >= 3000
+order by careerhits;
+
+
+/* 9. Find all players who had at least 1,000 hits for two different teams. Report those players' full names. */
+
+with highhitters as (select playerid, teamid, sum(h) as totalhits 
+from batting
+group by playerid, teamid
+having sum(h) >= 1000)
+select distinct p.namefirst || ' ' || p.namelast as playername
+from highhitters
+inner join people p
+using(playerid)
+group by playerid, playername
+having count(teamid) = 2;
+
+/* 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, 
+and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016. */
+with eligibleplayers as (
+(select playerid 
+from batting b
+group by playerid
+having max(yearid) - min(yearid) + 1 >= 10)
+intersect
+(select playerid
+from batting b
+where yearid = 2016
+and hr > 0)
+),
+careerhighhr as (
+select distinct on (playerid) playerid, yearid, hr as homeruns
+from batting b
+order by playerid, hr desc
+)
+select p.namefirst || ' ' || p.namelast as playername, yearid, homeruns
+from careerhighhr 
+inner join eligibleplayers
+using(playerid)
+inner join people p
+using(playerid)
+where yearid = 2016;
+
+
+
+
+
+
+
